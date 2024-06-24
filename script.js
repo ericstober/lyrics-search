06a97f83393a3bdf99da4a8650f466862c687b30
ericstober <erictstober@gmail.com>
2024-06-24 14:57:14 -0500

    update DOM with data & added pagination

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-const result = document.getElementById('result');
-const more = document.getElementById('more');
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const more = document.getElementById("more");
 
-const apiURL = 'https://api.lyrics.ovh';
+const apiURL = "https://api.lyrics.ovh";
 
 // Search API by song or artist
 async function searchSongs(term) {
@@ -15,30 +15,44 @@ async function searchSongs(term) {
 
 // Show song and artist data in DOM
 function showData(data) {
-  let output = '';
-
-  data.data.forEach(song => {
-    output += `
-      <li>
-        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
-      </li>
-    `;
-  });
-
   result.innerHTML = `
-    <ul class="songs">${output}</ul>
+    <ul class="songs">
+      ${data.data.map((song) => `
+        <li>
+          <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+          <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>`)
+        .join("")
+      }
+    </ul>
   `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Previous</button>` : ''}
+      ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ''}
+    `;
+  } else {
+    more.innerHTML = '';
+  }
+}
+
+// Get previous and next song results
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
 }
 
 // Event listeners
-form.addEventListener('submit', e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const searchTerm = search.value.trim();
 
   if (!searchTerm) {
-    alert('Please type in a search term');
+    alert("Please type in a search term");
   } else {
     searchSongs(searchTerm);
   }
